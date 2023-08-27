6d17e2c11ee512218ed699148ddc8f2a3d5a368e
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-27 21:28:37 +0530

    Adding Data Validation

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,5 @@
-// const mongoose = require("mongoose");
-// const mongoURI = "mongodb://localhost:27017";
-
-// const connectToMongo = () => {
-//   mongoose.connect(mongoURI, () => {
-//     console.log("Connected To Mongo");
-//   });
-// };
-
-// module.exports = connectToMongo
-
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI = "mongodb://localhost:27017/inotebook";
 
 const connectToMongo = async () => {
   try {
