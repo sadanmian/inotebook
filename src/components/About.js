edde08d1b82ca1785a0f44904fe987880897e6f6
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-29 10:39:02 +0530

    Context API

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import NoteContext from "../context/notes/noteContext";
 
 export default function About() {
-  return <div>About</div>;
+  const a = useContext(NoteContext);
+
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      This is about {a.state.name} and he has done his {a.state.class}
+    </div>
+  );
 }
