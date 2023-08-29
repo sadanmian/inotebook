68ea38f9276b2bbcb53daa4974828d8380b73eb8
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-29 14:14:46 +0530

    Adding AddNote

---
+++ src/components/AddNote.js
@@ -0,0 +1,75 @@
+import React, { useContext, useState } from "react";
+import NoteContext from "../context/notes/noteContext";
+
+export default function AddNote() {
+  const context = useContext(NoteContext);
+  const { addNote } = context;
+
+  const [note, setNote] = useState({
+    title: "",
+    description: "",
+    tag: "default",
+  });
+  const handleClick = (event) => {
+    event.preventDefault();
+    addNote(note.title, note.description, note.tag);
+  };
+
+  const onChange = (event) => {
+    setNote({
+      ...note,
+      [event.target.name]: event.target.value,
+    });
+  };
+  return (
+    <div>
+      <div className="container my-3">
+        <h2>Add a Note</h2>
+        <form className="my-3">
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
+              Title
+            </label>
+            <input
+              onChange={onChange}
+              type="text"
+              className="form-control"
+              id="title"
+              name="title"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description
+            </label>
+            <input
+              onChange={onChange}
+              type="text"
+              className="form-control"
+              id="description"
+              name="description"
+            />
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleClick}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -39,21 +39,32 @@ const NoteState = (props) => {
       date: "2023-08-29T05:55:33.005Z",
       __v: 0,
     },
-    {
-      _id: "64ed8822553a487d829b3d94b6",
-      user: "64eb969956d4c5ffa786a095",
-      title: "New My Title 2",
-      description: "Please get up early 2",
-      tag: "general 2",
-      date: "2023-08-29T05:55:33.005Z",
-      __v: 0,
-    },
   ];
 
   const [notes, setNotes] = useState(notesInitial);
 
+  // Add a note
+  const addNote = (title, description, tag) => {
+    const note = {
+      _id: "64ed8822553a487d829b3d94b6",
+      user: "64eb969956d4c5ffa786a095",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2023-08-29T05:55:33.005Z",
+      __v: 0,
+    };
+    setNotes(notes.concat(note));
+  };
+
+  // Delete a note
+  const deleteNote = (id) => {};
+
+  // Edit a note
+  const editNote = (id) => {};
+
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
       {props.children}
     </NoteContext.Provider>
   );
